388885ed34b901bbd5d6528f7dac84f4aa800940
iiTzAman <aman.giri2055@gmail.com>
2023-11-30 20:50:15 -0500

    Added test case for correct emails in local testing

---
+++ LocalTesting 2/app/src/test/java/jlin2/examples/localtesting/EmailValidatorLocalTesting.kt
@@ -0,0 +1,24 @@
+package jlin2.examples.localtesting
+
+import org.junit.Assert
+import org.junit.Test
+
+class EmailValidatorLocalTesting {
+    companion object {
+        val VALID_EMAIL_1 = "123@abc.com"
+        val VALID_EMAIL_2 = "123@abc.co.ca"
+        val INVALID_EMAIL_1 = "123@abc"
+        val INVALID_EMAIL_2 = "123@abc..com"
+        val INVALID_EMAIL_3 = "@abc.com"
+        val INVALID_EMAIL_4 = "testing123"
+        val EMPTY_EMAIL = ""
+    }
+    @Test
+    fun testDefaultEmailFormat() {
+        Assert.assertTrue(EmailValidator.isValidEmail(VALID_EMAIL_1))
+    }
+    @Test
+    fun testSubDomainFormat() {
+        Assert.assertTrue(EmailValidator.isValidEmail(VALID_EMAIL_2))
+    }
+}
